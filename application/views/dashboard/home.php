f5638e4312cd95feae717ff3bcb44dfbe084372c
Max Akana <quiksilvrgreen@gmail.com>
2013-02-02 13:10:49 +0530

    Set up directory structure for templates

--- application/views/dashboard/home.php
+++ application/views/dashboard/home.php
@@ -1,6 +1,9 @@
 		<div id="pageContent">
 			<h1>Dashboard</h1>
 			<p>
-				Okay, we put basic analytics here: Page views, total collected, total number of fundraisers etc.
+				General Settings here: Logo selection etc.
+			</p>
+			<p>
+				Basic analytics here: Page views, total collected, total number of fundraisers etc.
 			</p>
 		</div>

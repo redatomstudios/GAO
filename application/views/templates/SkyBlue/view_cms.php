6a98e73b05a495da6afb135dbdf885e64f893891
Max Akana <quiksilvrgreen@gmail.com>
2013-02-17 02:00:12 +0530

    Added navOrder and pageGroup in view_cms

--- application/views/templates/SkyBlue/view_cms.php
+++ application/views/templates/SkyBlue/view_cms.php
@@ -39,6 +39,28 @@
 						</td>
 					</tr>
 					<tr>
+						<td>
+							<label for="pageGroup">Page Group:</label>
+						</td>
+						<td>
+							<input type="text" name="pageGroup" id="pageGroup" placeholder="Navigation group, usually the same as the Page Name" />
+						</td>
+						<td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="navOrder">Navigation Order:</label>
+						</td>
+						<td>
+							<input type="text" name="navOrder" id="navOrder" placeholder="Navigation order, 1 is at the top. Use 0 to exclude from the navigation." />
+						</td>
+						<td>
+							
+						</td>
+					</tr>
+					<tr>
 						<td colspan="2">
 							<label for="pageContent">Page Content:</label>
 						</td>
